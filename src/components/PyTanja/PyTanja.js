cae00219c91d825aaeb5c61b1a6e9300711086de
Ensar Hamzic <ensar_hamzic@hotmail.com>
2022-10-22 19:26:53 +0200

    initial map added

--- src/components/PyTanja/PyTanja.js
+++ src/components/PyTanja/PyTanja.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
+import classes from "./PyTanja.module.css"
+
+// const initialGrid = [
+//   [null, null, null, null, null, null, null, null, null, null],
+//   [null, null, null, null, null, null, null, null, null, null],
+//   [null, null, null, null, null, null, null, null, null, null],
+//   [null, null, null, null, null, null, null, null, null, null],
+//   [null, null, null, null, null, null, null, null, null, null],
+//   [null, null, null, null, null, null, null, null, null, null],
+// ]
+let initialGrid = []
+
+for (let i = 0; i < 6; i++) {
+  let row = []
+  for (let j = 0; j < 10; j++) {
+    row.push(null)
+  }
+  initialGrid.push(row)
+}
 
 const PyTanja = () => {
-  return <div>PyTanja</div>
+  const [grid, setGrid] = useState(initialGrid)
+
+  return <div></div>
 }
 
 export default PyTanja
