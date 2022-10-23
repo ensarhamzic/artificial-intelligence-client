9b560a6cf75c87b220c6d62ccee843080f5c046d
Ensar <ensarhamzic01@gmail.com>
2022-10-23 20:44:54 +0200

    map size inputs

--- src/components/PyTanja/PyTanja.js
+++ src/components/PyTanja/PyTanja.js
@@ -26,6 +26,9 @@ const PyTanja = () => {
   const [map, setMap] = useState(defaultMap)
   const [choosenTile, setChoosenTile] = useState(null)
 
+  const [rowSize, setRowSize] = useState(6)
+  const [colSize, setColSize] = useState(10)
+
   const onTileSet = (row, col) => {
     if (!choosenTile || map[row][col] === choosenTile) return
     setMap((prevMap) => {
@@ -35,21 +38,65 @@ const PyTanja = () => {
     })
   }
 
-  const clearMap = () => {
+  const createEmptyMap = (rows, cols) => {
     const emptyMap = []
-    for (let i = 0; i < map.length; i++) {
+    for (let i = 0; i < rows; i++) {
       const row = []
-      for (let j = 0; j < map[0].length; j++) {
+      for (let j = 0; j < cols; j++) {
         row.push(null)
       }
       emptyMap.push(row)
     }
 
-    setMap(emptyMap)
+    return emptyMap
+  }
+
+  const clearMap = () => {
+    setMap(createEmptyMap(map.length, map[0].length))
+  }
+
+  const setMapSize = () => {
+    if (colSize < 3 || colSize > 15 || rowSize < 3 || rowSize > 15) return
+
+    const minCols = Math.min(colSize, map[0].length)
+    const minRows = Math.min(rowSize, map.length)
+
+    const newMap = createEmptyMap(rowSize, colSize)
+
+    for (let i = 0; i < minRows; i++) {
+      for (let j = 0; j < minCols; j++) {
+        newMap[i][j] = map[i][j]
+      }
+    }
+
+    setMap(newMap)
   }
 
   return (
     <div>
+      <div className={classes.mapSettings}>
+        <p>Map settings</p>
+        <div className={classes.mapSize}>
+          <input
+            type="number"
+            value={colSize}
+            onChange={(e) => {
+              setColSize(e.target.value)
+            }}
+          />
+          X
+          <input
+            type="number"
+            value={rowSize}
+            onChange={(e) => {
+              setRowSize(e.target.value)
+            }}
+          />
+          <button onClick={setMapSize}>Set Size</button>
+        </div>
+
+        <div className={classes.description}>Min: 3x3 | Max: 15x15</div>
+      </div>
       <PyTanjaMap map={map} onTileSet={onTileSet} />
       <PyTanjaTileChooser
         onChooseTile={(tile) => {
