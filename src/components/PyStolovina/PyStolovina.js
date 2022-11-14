905d9709a5d1f1c11bb7a6ad2142e952d06e7f7c
Ensar <ensarhamzic01@gmail.com>
2022-11-14 09:03:36 +0100

    started working on second part of the project

--- src/components/PyStolovina/PyStolovina.js
+++ src/components/PyStolovina/PyStolovina.js
@@ -1,7 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import MapSettings from "../MapSettings/MapSettings"
+import PyStolovinaMap from "../PyStolovinaMap/PyStolovinaMap"
+import classes from "./PyStolovina.module.css"
+
+const defaultMap = []
+
+for (let i = 0; i < 6; i++) {
+  const row = []
+  for (let j = 0; j < 10; j++) {
+    row.push("h")
+  }
+  defaultMap.push(row)
+}
 
 const PyStolovina = () => {
-  return <div>PyStolovina</div>
+  const [map, setMap] = useState(defaultMap)
+
+  const [mapRows, setMapRows] = useState(defaultMap.length)
+  const [mapCols, setMapCols] = useState(defaultMap[0].length)
+
+  const setMapSize = () => {}
+
+  return (
+    <div>
+      <MapSettings
+        rows={mapRows}
+        cols={mapCols}
+        onRowsChange={setMapRows}
+        onColsChange={setMapCols}
+        onSizeChange={setMapSize}
+      />
+
+      <PyStolovinaMap />
+    </div>
+  )
 }
 
 export default PyStolovina
---
+++ src/components/PyStolovinaMap/PyStolovinaMap.js
@@ -0,0 +1,8 @@
+import React from "react"
+import classes from "./PyStolovinaMap.module.css"
+
+const PyStolovinaMap = () => {
+  return <div>PyStolovinaMap</div>
+}
+
+export default PyStolovinaMap
