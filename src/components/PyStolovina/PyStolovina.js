38e360536709e4383f5b57a42093862c60b739c1
Ensar <ensarhamzic01@gmail.com>
2022-11-16 20:42:54 +0100

    map editor done

--- src/components/PyStolovina/PyStolovina.js
+++ src/components/PyStolovina/PyStolovina.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { NotificationManager } from "react-notifications"
 import MapSettings from "../MapSettings/MapSettings"
 import PyStolovinaMap from "../PyStolovinaMap/PyStolovinaMap"
 import classes from "./PyStolovina.module.css"
@@ -19,7 +20,51 @@ const PyStolovina = () => {
   const [mapRows, setMapRows] = useState(defaultMap.length)
   const [mapCols, setMapCols] = useState(defaultMap[0].length)
 
-  const setMapSize = () => {}
+  const createEmptyMap = (rows, cols) => {
+    const emptyMap = []
+    for (let i = 0; i < rows; i++) {
+      const row = []
+      for (let j = 0; j < cols; j++) {
+        row.push("h")
+      }
+      emptyMap.push(row)
+    }
+
+    return emptyMap
+  }
+
+  const setMapSize = () => {
+    // validation for map size
+    if (mapCols < 3 || mapCols > 10 || mapRows < 3 || mapRows > 10) {
+      NotificationManager.error("Invalid map size", "Error", 2000)
+      return
+    }
+
+    // if user clicks set map size with the same size as the current map
+    if (map.length === mapRows && map[0].length === mapCols) return
+
+    const minCols = Math.min(mapCols, map[0].length)
+    const minRows = Math.min(mapRows, map.length)
+
+    const newMap = createEmptyMap(mapRows, mapCols)
+
+    for (let i = 0; i < minRows; i++) {
+      for (let j = 0; j < minCols; j++) {
+        newMap[i][j] = map[i][j]
+      }
+    }
+
+    setMap(newMap)
+
+    NotificationManager.success("Map size changed", "Success", 2000)
+  }
+
+  const onTileChange = (row, col) => {
+    const newMap = [...map]
+    if (newMap[row][col] === "h") newMap[row][col] = "r"
+    else if (newMap[row][col] === "r") newMap[row][col] = "h"
+    setMap(newMap)
+  }
 
   return (
     <div>
@@ -31,7 +76,7 @@ const PyStolovina = () => {
         onSizeChange={setMapSize}
       />
 
-      <PyStolovinaMap map={map} />
+      <PyStolovinaMap map={map} onTileChange={onTileChange} />
     </div>
   )
 }
--- src/components/PyStolovinaMap/PyStolovinaMap.js
+++ src/components/PyStolovinaMap/PyStolovinaMap.js
@@ -1,30 +1,91 @@
-import React from "react"
+import React, { useState } from "react"
 import classes from "./PyStolovinaMap.module.css"
 
-const Tile = ({ tile, row, col }) => {
+const Tile = ({ tile, row, col, onTileChange, mouseDown }) => {
+  const [isChanged, setIsChanged] = useState(false)
+
   let tileClass = ""
   if (tile === "r") tileClass = classes.road
   if (tile === "h") tileClass = classes.hole
 
-  return <div className={`${classes.tile} ${tileClass}`}></div>
+  const changeTile = () => {
+    if (isChanged) return
+    onTileChange(row, col)
+  }
+
+  const mouseOverHandler = () => {
+    if (!mouseDown || isChanged) return
+    setIsChanged(true)
+    changeTile()
+  }
+
+  const mouseOutHandler = () => {
+    setIsChanged(false)
+  }
+
+  return (
+    <div
+      className={`${classes.tile} ${tileClass}`}
+      onContextMenu={changeTile}
+      onMouseMove={mouseOverHandler}
+      onMouseOut={mouseOutHandler}
+    ></div>
+  )
 }
 
-const MapRow = ({ tiles, row }) => {
+const MapRow = ({ tiles, row, onTileChange, mouseDown }) => {
   return (
     <div className={classes.row}>
       {tiles.map((f, i) => (
-        <Tile key={i} col={i} row={row} tile={f} />
+        <Tile
+          key={i}
+          col={i}
+          row={row}
+          tile={f}
+          onTileChange={onTileChange}
+          mouseDown={mouseDown}
+        />
       ))}
     </div>
   )
 }
 
-const PyStolovinaMap = ({ map }) => {
+const PyStolovinaMap = ({ map, onTileChange }) => {
+  const [mouseDown, setMouseDown] = useState(false)
+
+  const mouseDownHandler = (e) => {
+    if (e.button !== 2) return
+    setMouseDown(true)
+  }
+
+  const mouseUpHandler = (e) => {
+    if (e.button !== 2) return
+    setMouseDown(false)
+  }
+
+  const mouseLeaveHandler = () => {
+    setMouseDown(false)
+  }
+
   return (
-    <div className={classes.map}>
+    <div
+      className={classes.map}
+      onMouseDown={mouseDownHandler}
+      onMouseUp={mouseUpHandler}
+      onMouseLeave={mouseLeaveHandler}
+      onContextMenu={(e) => {
+        e.preventDefault()
+      }}
+    >
       <div>
         {map.map((rowTiles, i) => (
-          <MapRow key={i} tiles={rowTiles} row={i} />
+          <MapRow
+            key={i}
+            tiles={rowTiles}
+            row={i}
+            onTileChange={onTileChange}
+            mouseDown={mouseDown}
+          />
         ))}
       </div>
     </div>
